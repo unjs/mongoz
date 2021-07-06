a2470d991aa5477a2c0057873f72f9f6282bda7b
Pooya Parsa <pyapar@gmail.com>
2021-07-06 11:45:36 +0200

    chore: update mongo to latest

--- src/formula.ts
+++ src/formula.ts
@@ -1,21 +1,21 @@
 export const mongoFormula = {
   name: 'mongo',
-  version: '4.4.2',
+  version: '4.4.6',
   port: 27017,
   exec: 'bin/mongod',
   execArgs: '--port {port} --dbpath {data}',
   platforms: [
     {
       name: 'linux',
-      source: 'https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-ubuntu2004-4.4.2.tgz'
+      source: 'https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-ubuntu2004-4.4.6.tgz'
     },
     {
       name: 'win32',
-      source: 'https://fastdl.mongodb.org/windows/mongodb-windows-x86_64-4.4.2.zip'
+      source: 'https://fastdl.mongodb.org/windows/mongodb-windows-x86_64-4.4.6.zip'
     },
     {
       name: 'darwin',
-      source: 'https://fastdl.mongodb.org/osx/mongodb-macos-x86_64-4.4.2.tgz'
+      source: 'https://fastdl.mongodb.org/osx/mongodb-macos-x86_64-4.4.6.tgz'
     }
   ]
 }
